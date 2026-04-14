077280dea4c6493573c702804603529e1cb17cb9
Alex <alexanders823@gmail.com>
2026-04-14 09:47:59 +0300

    fixed errors

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,12 @@
 import { ReactNode } from 'react';
 
 import StreamVideoProvider from '@/providers/StreamClientProvider';
-import { Chat, useCreateChatClient } from 'stream-chat-react';
+// import { Chat, useCreateChatClient } from 'stream-chat-react';
 
-const apiKey = 'dz5f4d5kzrue';
-const userId = 'lingering-sun-0';
-const userName = 'lingering';
-const userToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoibGluZ2VyaW5nLXN1bi0wIiwiZXhwIjoxNzcyMDUzODczfQ.bKgYpcrdDBiXnVtRr9vhAuXo_ifSZ_2jh0RGtRvhF2Q';
+// const apiKey = 'dz5f4d5kzrue';
+// const userId = 'lingering-sun-0';
+// const userName = 'lingering';
+// const userToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoibGluZ2VyaW5nLXN1bi0wIiwiZXhwIjoxNzcyMDUzODczfQ.bKgYpcrdDBiXnVtRr9vhAuXo_ifSZ_2jh0RGtRvhF2Q';
 
 const RootLayout = ({ children }: Readonly<{ children: ReactNode }>) => {
 
