49628965f2bda51284e410e00f4b4f117cb8ca26
Alex <alexanders823@gmail.com>
2026-04-14 16:45:24 +0300

    fixed sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,10 +1,10 @@
 'use client';
 import { useState } from 'react';
-import Image from 'next/image';
+// import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
-import { sidebarLinks, colors } from '@/constants';
+import { sidebarLinks } from '@/constants';
 import { cn } from '@/lib/utils';
 
 const Sidebar = () => {
